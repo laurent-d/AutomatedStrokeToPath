73760983f7a63be1721851cbfbe57d071dc3c5d1
Laurent de Lacerda <lawz@free.fr>
2021-10-14 10:47:19 +0200

    Update stroketopath for Inscape 0.92 ++

--- stroketopath.sh
+++ stroketopath.sh
@@ -1,6 +1,8 @@
+rm -r $1"-stroketopath"
 mkdir $1"-stroketopath"
 for svgfile in $(ls $1)
 do
     cp "$1/"$svgfile $1"-stroketopath"
-    inkscape -f $1"-stroketopath/"$svgfile --verb="EditSelectAll" --verb="StrokeToPath" --verb="FileSave" --verb="FileQuit"
+    inkscape --with-gui $1"-stroketopath/"$svgfile --actions="EditSelectAll;StrokeToPath;FileSave;FileQuit"
+    echo "Processed $svgfile"
 done
